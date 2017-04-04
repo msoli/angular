9c0a3d6290271d6268b49277335e9231a189f6fc
DESKTOP-N99T78J\Miguel <miguel.solisp@gmail.com>
2017-04-03 23:45:23 -0500

    hola mundo

---
+++ 1.HolaMundo/src/app/app.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'my-app',
+  template: `<h1>{{name}}</h1>`,
+})
+export class AppComponent {
+
+  name = 'Hola Mundo';
+
+
+}
