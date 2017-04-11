5af9d349441a7bd2a61ecf53593431e59ea6a21f
DESKTOP-N99T78J\Miguel <miguel.solisp@gmail.com>
2017-04-11 00:13:14 -0500

    router

--- 2.SPA/src/app/app.routes.ts
+++ 2.SPA/src/app/app.routes.ts
@@ -4,9 +4,11 @@ import {Routes, RouterModule} from '@angular/router';
 import {HomeComponent} from './components/home/home.component';
 import {HeroesComponent} from './components/heroes/heroes.component';
 import {AboutComponent} from './components/about/about.component';
+import {HeroeComponent} from './components/heroe/heroe.component';
 
 const APP_ROUTES: Routes = [
   {path: 'home', component: HomeComponent},
+  {path: 'heroe/:id', component: HeroeComponent},
   {path: 'heroes', component: HeroesComponent},
   {path: 'about', component: AboutComponent},
   {path: '**', pathMatch: 'full', redirectTo: 'home'}
--- 2.SPA/src/app/components/heroes/heroes.component.ts
+++ 2.SPA/src/app/components/heroes/heroes.component.ts
@@ -3,6 +3,8 @@ import {Component, OnInit} from '@angular/core';
 import {Hero, HeroesService} from '../../servicios/heroes.service';
 import 'rxjs/add/operator/map';
 
+import {Router} from '@angular/router';
+
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
@@ -12,13 +14,18 @@ export class HeroesComponent implements OnInit {
 
   heroes: Hero[] = [];
 
-  constructor(private heroService: HeroesService) {
+  constructor(private heroService: HeroesService,
+              private router: Router) {
   }
 
   ngOnInit() {
     // this.heroService.getHeroesProm().then(heroes => this.heroes = heroes);
     this.heroService.getHeroesObs().subscribe(heroes => this.heroes = heroes);
-    setTimeout(() => console.log(this.heroes), 2000);
+    // setTimeout(() => console.log(this.heroes), 2000);
+  }
+
+  verHeroe(index: number) {
+    this.router.navigate(['/heroe', index]);
   }
 
 }
