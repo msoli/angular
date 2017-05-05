32044c68a1bbe6a52aba703a22e71eb1e166f424
DESKTOP-N99T78J\Miguel <miguel.solisp@gmail.com>
2017-05-05 00:05:53 -0500

    home, navbar, search components

--- 4.SpotiApp/src/app/app.module.ts
+++ 4.SpotiApp/src/app/app.module.ts
@@ -4,10 +4,16 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { SearchComponent } from './components/search/search.component';
+import { NavbarComponent } from './components/shared/navbar/navbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    SearchComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
