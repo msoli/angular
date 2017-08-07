901077e016abd56e8c1864d515ea891dad54b9db
Miguel Solis <miguel.solis@clickfactura.mx>
2017-08-07 10:23:28 -0500

    listas

---
+++ 5.deseos.ionic/src/app/clases/listas.ts
@@ -0,0 +1,15 @@
+import {ListaItem} from './lista-item';
+
+export class Lista {
+  nombre: string;
+  terminada: boolean;
+  items: ListaItem[];
+
+  constructor(nombre: string) {
+
+    this.nombre = nombre;
+    this.terminada = false;
+  }
+
+
+}
--- 5.deseos.ionic/src/app/services/lista-deseos.service.ts
+++ 5.deseos.ionic/src/app/services/lista-deseos.service.ts
@@ -1,9 +1,21 @@
 import {Injectable} from '@angular/core';
+import {ListaItem} from '../clases/lista-item';
+import {Lista} from '../clases/listas';
 
 @Injectable()
 export class ListaDeseosService {
 
+  listas: Lista[] = [];
+
   constructor() {
+    let lista1 = new Lista('Compras supermercado');
+    let lista2 = new Lista('Juegos que deseo');
+    let lista3 = new Lista('Cosas de la universidad');
+
+    this.listas.push(lista1);
+    this.listas.push(lista2);
+    this.listas.push(lista3);
+
     console.log('Servicio inicializado');
   }
 }
