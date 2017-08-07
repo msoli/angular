8a6040342bdda755538b5d73bade2ea804ef5a42
DESKTOP-N99T78J\Miguel <miguel.solisp@gmail.com>
2017-08-06 23:00:55 -0500

    5 ionic deseos

---
+++ 5.deseos.ionic/src/app/app.module.ts
@@ -0,0 +1,44 @@
+import {NgModule, ErrorHandler} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {IonicApp, IonicModule, IonicErrorHandler} from 'ionic-angular';
+import {MyApp} from './app.component';
+
+import {ContactPage} from '../pages/contact/contact';
+import {TabsPage} from '../pages/tabs/tabs';
+
+import {StatusBar} from '@ionic-native/status-bar';
+import {SplashScreen} from '@ionic-native/splash-screen';
+
+import {PendientesComponent} from '../pages/pendientes/pendientes.component';
+import {TerminadosComponent} from "../pages/termiados/terminados.component";
+import {ListaDeseosService} from "./services/lista-deseos.service";
+
+@NgModule({
+  declarations: [
+    MyApp,
+    ContactPage,
+    PendientesComponent,
+    TerminadosComponent,
+    TabsPage
+  ],
+  imports: [
+    BrowserModule,
+    IonicModule.forRoot(MyApp)
+  ],
+  bootstrap: [IonicApp],
+  entryComponents: [
+    MyApp,
+    ContactPage,
+    TabsPage,
+    PendientesComponent,
+    TerminadosComponent
+  ],
+  providers: [
+    StatusBar,
+    SplashScreen,
+    ListaDeseosService,
+    {provide: ErrorHandler, useClass: IonicErrorHandler}
+  ]
+})
+export class AppModule {
+}
---
+++ 5.deseos.ionic/src/app/services/lista-deseos.service.ts
@@ -0,0 +1,9 @@
+import {Injectable} from '@angular/core';
+
+@Injectable()
+export class ListaDeseosService {
+
+  constructor() {
+    console.log('Servicio inicializado');
+  }
+}
