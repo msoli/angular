a50e6ad31c8c96d56acb7fc0fa80d557a84967e4
DESKTOP-N99T78J\Miguel <miguel.solisp@gmail.com>
2017-08-19 20:46:29 -0500

    rutas hijas

--- 6.miscelaneos/src/app/components/home/home.component.ts
+++ 6.miscelaneos/src/app/components/home/home.component.ts
@@ -1,34 +1,69 @@
-import {Component, OnInit} from '@angular/core';
+import {
+  Component, OnInit, OnChanges, DoCheck, AfterContentInit,
+  AfterContentChecked, AfterViewInit, AfterViewChecked, OnDestroy, SimpleChanges
+} from '@angular/core';
 
 @Component({
   selector: 'app-home',
   template: `
 
     <app-ng-style></app-ng-style>
-    
+
     <hr>
     <app-css></app-css>
-    
+
     <hr>
     <app-clases></app-clases>
-    
+
     <hr>
     <p [appResaltado]="'blue'">
       Hola Directiva
     </p>
-    
+
     <hr>
     <app-ng-switch></app-ng-switch>
 
   `,
   styles: []
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent implements OnInit, OnChanges, DoCheck, AfterContentInit,
+  AfterContentChecked, AfterViewInit, AfterViewChecked, OnDestroy {
 
   constructor() {
+    console.log('constructor');
   }
 
   ngOnInit() {
+    console.log('ngOnInit');
   }
+
+  ngOnChanges(): void {
+    console.log('ngOnChanges');
+  }
+
+  ngDoCheck(): void {
+    console.log('ngDoCheck');
+  }
+
+  ngAfterContentInit(): void {
+    console.log('ngAfterContentInit');
+  }
+
+  ngAfterContentChecked(): void {
+    console.log('ngAfterContentChecked');
+  }
+
+  ngAfterViewInit(): void {
+    console.log('ngAfterViewInit');
+  }
+
+  ngAfterViewChecked(): void {
+    console.log('ngAfterViewChecked');
+  }
+
+  ngOnDestroy(): void {
+    console.log('ngOnDestroy');
+  }
+
 
 }
--- 6.miscelaneos/src/app/components/usuario/usuario.routes.ts
+++ 6.miscelaneos/src/app/components/usuario/usuario.routes.ts
@@ -1,9 +1,15 @@
-import {Routes, RouterModule} from '@angular/router';
+import {Routes} from '@angular/router';
+
+import {UsuarioNuevoComponent} from './usuario-nuevo.component';
+import {UsuarioDetalleComponent} from './usuario-detalle.component';
+import {UsuarioEditarComponent} from './usuario-editar.component';
 
 
-export const routes: Routes = [
-  {path: 'path', component: NameComponent},
-  {path: '**', pathMatch: 'full', redirectTo: 'home'},
+export const USUARIO_ROUTES: Routes = [
+  {path: 'nuevo', component: UsuarioNuevoComponent},
+  {path: 'detalle', component: UsuarioDetalleComponent},
+  {path: 'editar', component: UsuarioEditarComponent},
+  {path: '**', pathMatch: 'full', redirectTo: 'nuevo'}
 
 ];
 
