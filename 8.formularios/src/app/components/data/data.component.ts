f8328672c8a7b9210a649303d041d6e60428699b
DESKTOP-N99T78J\Miguel <miguel.solisp@gmail.com>
2017-09-04 23:59:13 -0500

    custom validators

--- 8.formularios/src/app/components/data/data.component.ts
+++ 8.formularios/src/app/components/data/data.component.ts
@@ -1,5 +1,5 @@
 import {Component, OnInit} from '@angular/core';
-import {FormGroup, FormControl, Validators} from '@angular/forms';
+import {FormGroup, FormControl, Validators, FormArray} from '@angular/forms';
 
 @Component({
   selector: 'app-data',
@@ -15,7 +15,8 @@ export class DataComponent implements OnInit {
       nombre: 'Miguel',
       apellido: 'Solis'
     },
-    correo: 'miguel.solis@mail.com'
+    correo: 'miguel.solis@mail.com',
+    // pasatiempos: []
   };
 
   constructor() {
@@ -29,33 +30,81 @@ export class DataComponent implements OnInit {
           Validators.required,
           Validators.minLength(3)
         ]),
-        'apellido': new FormControl('', Validators.required),
+        'apellido': new FormControl('', [
+          Validators.required,
+          this.noHerrera
+        ]),
 
       }),
       'correo': new FormControl('', [
         Validators.required,
         Validators.pattern('[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3}$')
       ]),
+      'pasatiempos': new FormArray([
+        new FormControl('Correr', Validators.required)
+      ]),
+      'password1': new FormControl('', Validators.required),
+      'password2': new FormControl(),
+
     });
 
-    this.forma.setValue(this.usuario);
+    this.forma.controls['password2'].setValidators([
+      Validators.required, this.noIgual.bind(this.forma)
+    ]);
+
+    // this.forma.setValue(this.usuario);
 
   }
 
   ngOnInit() {
   }
 
+  agregarPasatiempo(): void {
+
+    (<FormArray>this.forma.controls['pasatiempos']).push(
+      new FormControl('Dormir', Validators.required)
+    );
+
+  }
+
+
+  noHerrera(control: FormControl): { [s: string]: boolean } {
+
+    if (control.value === 'herrera') {
+      return {
+        noherrera: true
+      };
+    }
+
+    return null;
+
+  }
+
+  noIgual(control: FormControl): { [s: string]: boolean } {
+
+    let forma: any = this;
+
+    if (control.value !== forma.controls['password1'].value) {
+      return {
+        noiguales: true
+      };
+    }
+
+    return null;
+
+  }
+
   guardarCambio(): void {
     console.log(this.forma);
     console.log(this.forma.value);
 
-    this.forma.reset({
-      nombrecompleto: {
-        nombre: '',
-        apellido: ''
-      },
-      correo: ''
-    });
+    // this.forma.reset({
+    //   nombrecompleto: {
+    //     nombre: '',
+    //     apellido: ''
+    //   },
+    //   correo: ''
+    // });
 
     // this.forma.controls['correo'].setValue('correo@mai.com');
 
